14bff2ca81f6b4fee0d8375ae3a60f636bffe772
SabaNasirii <107081515+SabaNasirii@users.noreply.github.com.>
2024-12-18 13:23:27 +0100

    adding run_svr

--- run_svr.py
+++ run_svr.py
@@ -1,6 +1,16 @@
-def main() -> None:
-    pass
+from data_loader import get_inputs_my_personality, get_inputs_data, get_inputs_chunked_data
+from learning import Regression_SVR
 
-
-if __name__ == "__main__":
-    main()
+def run_svr(type, datapath_features, datapath_targets, target, features, datapath_features2 = None):
+    if type == 'my_personality':
+        X_my_personality, y_my_personality = get_inputs_my_personality(datapath_features = datapath_features, datapath_targets = datapath_targets, features = features, datapath_features2 = datapath_features2)
+        Regression_SVR(X = X_my_personality, y = y_my_personality, type = 'my_personality',  target = target, features = features)
+        
+    elif type == 'idiap':
+        X_idiap, y_idiap = get_inputs_data(datapath_features = datapath_features, datapath_targets = datapath_targets, features = features, datapath_features2 = datapath_features2)
+        Regression_SVR(X = X_idiap, y = y_idiap, type = 'idiap',  target = target, features = features)    
+    
+    
+    elif type == 'idiap_chunked':
+        X_idiap, y_idiap = get_inputs_chunked_data(datapath_features = datapath_features, datapath_targets = datapath_targets, features = features, datapath_features2 = datapath_features2)
+        Regression_SVR(X = X_idiap, y = y_idiap, type = 'idiap_chunked',  target = target, features = features)
